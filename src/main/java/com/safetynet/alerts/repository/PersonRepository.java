69f7745368859be01ea3a4431e4009f8f601e869
toastnjamz <veritydavis@gmail.com>
2019-12-11 18:54:39 -0800

    updating PersonService

--- src/main/java/com/safetynet/alerts/repository/PersonRepository.java
+++ src/main/java/com/safetynet/alerts/repository/PersonRepository.java
@@ -1,8 +1,8 @@
 package com.safetynet.alerts.repository;
-import com.safetynet.alerts.domain.Person;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.safetynet.alerts.domain.Person;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, String> {
--- src/main/java/com/safetynet/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/alerts/service/PersonService.java
@@ -1,5 +1,15 @@
 package com.safetynet.alerts.service;
 
+import org.springframework.stereotype.Service;
+
+import com.safetynet.alerts.repository.PersonRepository;
+
+@Service
 public class PersonService {
+	private PersonRepository personRepository;
+	
+	public PersonService(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 }
