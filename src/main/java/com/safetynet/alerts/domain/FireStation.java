fb08bcab78778c88a5bb38da43c3019f17fae193
toastnjamz <veritydavis@gmail.com>
2020-01-30 21:46:06 -0800

    switching to IntelliJ, adding ApplicationReadyEvent to loader

---
+++ src/main/java/com/safetynet/alerts/domain/FireStation.java
@@ -0,0 +1,30 @@
+package com.safetynet.alerts.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FireStation {
+
+    private Set<String> addresses = new HashSet<String>();
+    private String stationNo;
+
+    public FireStation(String stationNo) {
+        this.stationNo = stationNo;
+    }
+
+    public Set<String> getAddresses() {
+        return addresses;
+    }
+
+    public void addAddress(String address) {
+        addresses.add(address);
+    }
+
+    public String getStationNo() {
+        return stationNo;
+    }
+
+    public void setStationNo(String stationNo) {
+        this.stationNo = stationNo;
+    }
+}
--- src/main/java/com/safetynet/alerts/jsonloader/JsonLoader.java
+++ src/main/java/com/safetynet/alerts/jsonloader/JsonLoader.java
@@ -7,24 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.jsoniter.JsonIterator;
 import com.jsoniter.any.Any;
 import com.safetynet.alerts.domain.Person;
+import com.safetynet.alerts.repository.PersonRepository;
 
-@Service
-public class JsonLoader implements ApplicationEventPublisherAware {
+@Component
+public class JsonLoader {
 
 	@Autowired
-	private List<Person> personList = new ArrayList<Person>();
-	
-	private ApplicationEventPublisher publisher;
-	
-	// Constructor deserializes data from JSON file
-	public JsonLoader() throws IOException {
+	private PersonRepository personRepository;
+
+	@EventListener
+	public void onApplicationReadyEvent(ApplicationReadyEvent event) throws IOException {
 		String jsonFilePath = "src/main/resources/data.json";
 		byte[] byteArray;
 		byteArray = Files.readAllBytes(new File(jsonFilePath).toPath());
@@ -33,24 +33,12 @@ public class JsonLoader implements ApplicationEventPublisherAware {
 		// Any is a lazy container in Jsoniter that can hold different values
 		Any any = jsoniter.readAny();
 		Any anyPerson = any.get("persons");
-		anyPerson.forEach(p -> personList.add(new Person(p.get("firstName").toString(),
+		anyPerson.forEach(p -> personRepository.createPerson(new Person(p.get("firstName").toString(),
 				(p.get("lastName").toString()),
 				(p.get("address").toString()),
 				(p.get("city").toString()),
 				(p.get("zip").toString()),
 				(p.get("phone").toString()),
 				(p.get("email").toString()))));
-		
-		
-		publisher.publishEvent(new JsonLoaderEvent(this, "Loading JSON", this));
-	}
-		
-	@Override
-	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
-		this.publisher = publisher;
-	}
-	
-	public List<Person> getPersons() {
-		return personList;
 	}
 }
