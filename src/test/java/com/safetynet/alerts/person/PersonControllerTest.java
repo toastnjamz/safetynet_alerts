ea425788177e65fddc0040056a3cea6cededbcbb
toastnjamz <veritydavis@gmail.com>
2020-02-05 08:20:14 -0800

    Updating PersonServiceTest with new exception unit tests

---
+++ src/test/java/com/safetynet/alerts/person/PersonControllerTest.java
@@ -0,0 +1,92 @@
+package com.safetynet.alerts.person;
+
+import com.safetynet.alerts.controller.PersonController;
+import com.safetynet.alerts.service.PersonService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//@WebMvcTest(PersonController.class)
+//@WebMvcTest only loads controller beans to test handler methods
+@WebMvcTest
+//@AutoConfigureMockMvc
+//@SpringBootTest
+@RunWith(SpringRunner.class)
+public class PersonControllerTest {
+
+    //Acts as a dummy web browser
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    @MockBean
+    private PersonService personService;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
+    @Test
+    public void getAllPerson_personsExist_returnsAllPersons() throws Exception {
+//        mockMvc.perform(get("/persons"))
+//                .andExpect(status().is2xxSuccessful())
+//                .andExpect(content().json("[{\"firstName\":\"John\",\"lastName\":\"Boyd\"}]"));
+    }
+
+    @Test
+    public void getPersonByFirstLastName_personExists_returnsPerson() {
+        //TODO
+    }
+
+    @Test
+    public void getPersonByFirstLastName_personDoesNotExist_throwsPersonNotFoundException() {
+        //TODO
+    }
+
+    @Test
+    public void createPerson_personDoesNotAlreadyExist_returnsPerson() {
+        //TODO
+    }
+
+    @Test
+    public void createPerson_personExists_throwsDuplicatePersonException() {
+        //TODO
+    }
+
+    @Test
+    public void updatePerson_personExists_updatesPerson() {
+        //TODO
+    }
+
+    @Test
+    public void updatePerson_personDoesNotExist_throwsPersonNotFoundException() {
+        //TODO
+    }
+
+    @Test
+    public void deletePerson_personExists_deletesPerson() {
+        //TODO
+    }
+
+    @Test
+    public void deletePerson_personDoesNotExist_throwsPersonNotFoundException() {
+        //TODO
+    }
+}
--- src/test/java/com/safetynet/alerts/person/PersonServiceTest.java
+++ src/test/java/com/safetynet/alerts/person/PersonServiceTest.java
@@ -2,38 +2,19 @@ package com.safetynet.alerts.person;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 
-import java.util.Arrays;
-import java.util.List;
-
+import com.safetynet.alerts.exception.DuplicatePersonException;
+import com.safetynet.alerts.exception.PersonNotFoundException;
 import org.hamcrest.CoreMatchers;
-import org.json.JSONException;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.skyscreamer.jsonassert.JSONAssert;
 
 import com.safetynet.alerts.domain.Person;
-import com.safetynet.alerts.jsonloader.JsonLoader;
 import com.safetynet.alerts.repository.PersonRepository;
 import com.safetynet.alerts.service.PersonService;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(MockitoJUnitRunner.class)
 @RunWith(SpringRunner.class)
 public class PersonServiceTest {
 
@@ -54,59 +35,6 @@ public class PersonServiceTest {
 		personService.createPerson(person2);
 	}
 
-//	@Mock
-//	private PersonRepository personRepository;
-//
-//	@InjectMocks
-//	private PersonService personService;
-	
-//	private PersonRepository personRepository = new mock(PersonRepository.class);
-//	private PersonService personService = new PersonService(personRepository);
-	
-//	@Before
-//	public void setup() {
-//		MockitoAnnotations.initMocks(this);
-//	}
-//	
-//	private JsonLoader jsonLoader;
-//	private PersonRepository personRepository;
-//	private PersonService personService;
-//	
-//	@Before
-//	public void setup() {
-//		jsonLoader = mock(JsonLoader.class);
-//		personRepository = mock(PersonRepository.class);
-//		personService = new PersonService(personRepository);
-//	}
-	
-//	@Spy
-//	private JsonLoader jsonLoader;
-//	
-//	@Spy @InjectMocks
-//	private PersonRepository personRepository = new PersonRepository(jsonLoader);
-//	private PersonRepository personRepository = spy(PersonRepository.class);
-//	private PersonRepository personRepository;
-	
-//	@Test
-//	public void getAllPersons_repositoryHasData_allDataReturned() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//		Person person2 = new Person("Jessica", "Atreides", "456 Grand Palace", "Arrakeen", "94501",
-//				"555-555-5555", "benegesserit@yahoo.com");
-//
-//		when(personRepository.findAll()).thenReturn(Arrays.asList(person1, person2));
-//
-//		// act
-//		String result = personService.getAllPersons();
-//
-//		// assert
-//		assertThat(result, CoreMatchers.containsString("Duncan"));
-//		assertThat(result, CoreMatchers.containsString("Jessica"));
-//		assertEquals(2, personRepository.findAll().size());
-//		//verify(personRepository).findAll();
-//	}
-
 	@Test
 	public void getAllPersons_repositoryHasData_allDataReturned() {
 		// arrange
@@ -119,41 +47,6 @@ public class PersonServiceTest {
 		assertThat(result, CoreMatchers.containsString("Jessica"));
 		assertEquals(2, personRepository.findAll().size());
 	}
-	
-//	@Test
-//	public void getPersonByFirstLastName_repositoryHasData_personDataReturned() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//
-//		when(personRepository.findPerson("Duncan", "Idaho")).thenReturn(person1);
-//
-//		// act
-//		String foundPerson = personService.getPersonByFirstLastName(person1.getFirstName(), person1.getLastName());
-//
-//		// assert
-//		assertThat(foundPerson, CoreMatchers.containsString("Duncan"));
-//		assertThat(foundPerson, CoreMatchers.containsString("Duncan"));
-//		assertEquals(1, personRepository.findAll().size());
-//	}
-
-//	@Test
-//	public void getPersonByFirstLastName_repositoryHasData_personDataReturned() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//
-//		personRepository.createPerson(person1);
-//
-//		when(personRepository.findPerson("Duncan", "Idaho")).thenReturn(person1);
-//
-//		// act
-//		String foundPerson = personService.getPersonByFirstLastName("Duncan", "Idaho");
-//
-//		// assert
-//		assertThat(foundPerson, CoreMatchers.containsString("Giedi Prime"));
-//		assertEquals(1, personRepository.findAll().size());
-//	}
 
 	@Test
 	public void getPersonByFirstLastName_repositoryHasData_personDataReturned() {
@@ -165,41 +58,19 @@ public class PersonServiceTest {
 		// assert
 		assertThat(foundPerson, CoreMatchers.containsString("Giedi Prime"));
 	}
-	
-//	@Test
-//	public void createPerson_addingOnePerson_personDataReturned() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//
-//		when(personRepository.createPerson(person1)).thenReturn(person1);
-//
-//		// act
-//		String personCreated = personService.createPerson(person1);
-//
-//		// assert
-//		assertThat(personCreated, CoreMatchers.containsString("Duncan"));
-//		assertEquals(1, personRepository.findAll().size());
-//	}
 
-//	@Test
-//	public void createPerson_addingOnePerson_personDataReturned() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//
-//		when(personRepository.createPerson(person1)).thenReturn(person1);
-//
-//		// act
-//		String personCreated = personService.createPerson(person1);
-//
-//		// assert
-//		assertThat(personCreated, CoreMatchers.containsString("Duncan"));
-//		assertEquals(1, personRepository.findAll().size());
-//	}
+	@Test (expected = PersonNotFoundException.class)
+	public void getPersonByFirstLastName_personFirstLastNameNull_throwsPersonNotFoundException() {
+		// arrange
+
+		// act
+		String foundPerson = personService.getPersonByFirstLastName("Duncan", "Atreides");
+
+		// assert
+	}
 
 	@Test
-	public void createPerson_addingOnePerson_personDataReturned() {
+	public void createPerson_addingNewPerson_personDataReturned() {
 		// arrange
 		Person person3 = new Person("Paul", "Atreides", "123 Grand Palace", "Caladan",
 				"12345", "555-555-5555", "maud-dib@gmail.com");
@@ -210,6 +81,18 @@ public class PersonServiceTest {
 		// assert
 		assertThat(personCreated, CoreMatchers.containsString("Paul"));
 		assertEquals(3, personRepository.findAll().size());
+	}
+
+	@Test (expected = DuplicatePersonException.class)
+	public void createPerson_addingExistingPerson_throwsDuplicatePersonException() {
+		// arrange
+		Person person3 = new Person("Duncan", "Idaho", "123 Market Street", "Giedi Prime",
+				"12345", "555-555-5555", "duncan@gmail.com");
+
+		// act
+		String personCreated = personService.createPerson(person3);
+
+		// assert
 	}
 
 	@Test
@@ -226,22 +109,18 @@ public class PersonServiceTest {
 		assertThat(personUpdated, CoreMatchers.containsString("456 Sietch Tabr"));
 		assertThat(personUpdated, CoreMatchers.containsString("shaihulud@gmail.com"));
 	}
-	
-//	@Test
-//	public void deletePerson_addingAndDeletingPerson_personDataDeleted() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//
-//		when(personRepository.createPerson(person1)).thenReturn(person1);
-//
-//		// act
-//		String personCreated = personService.createPerson(person1);
-//
-//		// assert
-//		assertThat(personCreated, CoreMatchers.containsString("Duncan"));
-//		assertEquals(false, personRepository.findAll().isEmpty());
-//	}
+
+	@Test (expected = PersonNotFoundException.class)
+	public void updatePerson_updatingNewPerson_throwsPersonNotFoundException() {
+		// arrange
+		Person person3 = new Person("Paul", "Atreides", "123 Grand Palace", "Caladan",
+				"12345", "555-555-5555", "maud-dib@gmail.com");
+
+		// act
+		personService.updatePerson(person3);
+
+		// assert
+	}
 
 	@Test
 	public void deletePerson_addingAndDeletingPerson_personDataDeleted() {
@@ -259,50 +138,14 @@ public class PersonServiceTest {
 		// assert
 		assertEquals(2, personRepository.findAll().size());
 	}
-	
-//	@Test
-//	public void deletePerson_addingAndDeletingPerson_personDataDeleted() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//		
-//		ArgumentCaptor<String> firstNameCaptor = ArgumentCaptor.forClass(String.class);
-//		ArgumentCaptor<String> lastNameCaptor = ArgumentCaptor.forClass(String.class);
-//		
-//		when(personRepositoryMock.createPerson(person1)).thenReturn(person1);
-//		personService.createPerson(person1);
-//		
-//		// act
-//		assertEquals(false, personRepositoryMock.findAll().isEmpty());
-//		personService.deletePerson(person1.getFirstName(), person1.getLastName());
-//		assertEquals(true, personRepositoryMock.findAll().isEmpty());
-//		
-//		// assert
-//		verify(personRepositoryMock, times(1)).deletePerson(firstNameCaptor.capture(), lastNameCaptor.capture());
-//		assertEquals("Duncan", firstNameCaptor.getValue());
-//		assertEquals("Idaho", lastNameCaptor.getValue());
-//	}
-	
-//	@Test
-//	public void deletePerson_addingAndDeletingPerson_personDataDeleted() {
-//		// arrange
-//		Person person1 = new Person("Duncan", "Idaho", "123 Harkonnen Street", "Giedi Prime",
-//				"94501", "555-555-5555", "duncan@gmail.com");
-//		
-//		ArgumentCaptor<String> firstNameCaptor = ArgumentCaptor.forClass(String.class);
-//		ArgumentCaptor<String> lastNameCaptor = ArgumentCaptor.forClass(String.class);
-//		
-//		when(personRepositoryMock.createPerson(person1)).thenReturn(person1);
-//		personService.createPerson(person1);
-//		
-//		doNothing().when(personRepositoryMock).deletePerson(firstNameCaptor.capture(), lastNameCaptor.capture());
-//		
-//		// act
-//		personService.deletePerson(person1.getFirstName(), person1.getLastName());
-//		
-//		// assert
-//		verify(personRepositoryMock, times(1)).deletePerson(firstNameCaptor.capture(), lastNameCaptor.capture());
-//		assertEquals("Duncan", firstNameCaptor.getValue());
-//		assertEquals("Idaho", lastNameCaptor.getValue());
-//	}
+
+	@Test (expected = PersonNotFoundException.class)
+	public void deletePerson_deletingNewPerson_throwsPersonNotFoundException() {
+		// arrange
+
+		// act
+		personService.deletePerson("Paul", "Atreides");
+
+		// assert
+	}
 }
