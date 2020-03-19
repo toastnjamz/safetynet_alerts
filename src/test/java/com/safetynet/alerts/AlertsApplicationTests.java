a53d15b6f4eaa35a6aeb0996ac446c21d6cb693b
toastnjamz <veritydavis@gmail.com>
2020-03-18 20:56:05 -0700

    updating tests file to work with older version of spring boot

--- src/test/java/com/safetynet/alerts/AlertsApplicationTests.java
+++ src/test/java/com/safetynet/alerts/AlertsApplicationTests.java
@@ -1,13 +1,15 @@
 package com.safetynet.alerts;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlertsApplicationTests {
+public class AlertsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
-
 }
